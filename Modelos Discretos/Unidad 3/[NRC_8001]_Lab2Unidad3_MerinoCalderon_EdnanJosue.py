fde04f330838eaec4c61f6d5b4f294c32e132717
Josué Merino Calderón <63412435+ejmerino@users.noreply.github.com>
2023-02-08 20:22:45 -0500

    Cambios

--- Modelos Discretos/Unidad 3/[NRC_8001]_Lab2Unidad3_MerinoCalderon_EdnanJosue.py
+++ Modelos Discretos/Unidad 3/[NRC_8001]_Lab2Unidad3_MerinoCalderon_EdnanJosue.py
@@ -6,7 +6,9 @@ Autor: Ednan Josué Merino Calderón
 Versión: 1.1
 '''
 
-"""_busqueda_centro_historico_
+def busqueda_general(objetivo, inicio):
+	"""
+	Función que ayuda a encontrar la lista de costos mínimos en un grafo, a partir de un nodo de inicio.
 
 	Args:
 		goal (_type_): es una lista de nodos deseados
@@ -14,70 +16,114 @@ Versión: 1.1
 
 	Returns:
 		_type_: lista de costos mínimos desde el nodo de inicio hasta cada nodo en la lista de objetivos.
-"""
-def busqueda_centro_historico(goal, start):
-    # Declaración de variables globales para el grafo y los costos
-    global graph, cost
-    
-    # Lista para almacenar los costos mínimos
-    answer = []
-    # Cola para expandir los nodos
-    queue = []
-    # Inicialización de la lista de respuestas con un valor alto
-    for i in range(len(goal)):
-        answer.append(10**8)
-    # Agregar el nodo de inicio a la cola con costo 0
-    queue.append([0, start])
-    # Diccionario para almacenar los nodos visitados
-    visited = {}
-    # Contador para verificar cuántos nodos objetivo se han encontrado
-    count = 0
-    # Mientras la cola no esté vacía
-    while len(queue) > 0:
-        # Ordenar la cola por costo
-        queue = sorted(queue, key=lambda x: x[0])
-        # Tomar el nodo con el costo mínimo
-        p = queue.pop(0)
-        # Si el nodo actual es un nodo objetivo
-        if p[1] in goal:
-            # Obtener el índice del nodo objetivo en la lista de objetivos
-            index = goal.index(p[1])
-            # Si aún no se ha encontrado un costo mínimo para este nodo objetivo
-            if answer[index] == 10**8:
-                # Incrementar el contador
-                count += 1
-            # Si el costo actual es menor que el costo mínimo previamente encontrado para este nodo objetivo
-            if answer[index] > p[0]:
-                # Actualizar el costo mínimo
-                answer[index] = p[0]
-            # Si se han encontrado costos mínimos para todos los nodos objetivo
-            if count == len(goal):
-                # Devolver la lista de costos mínimos
-                return answer
-        # Si el nodo actual aún no ha sido visitado
-        if p[1] not in visited:
-            # Expandir sus vecinos
-            for i in graph[p[1]]:
-                queue.append([p[0] + cost[(p[1], i)], i])
-        # Marcar el nodo actual como visitado
-        visited[p[1]] = 1
-    # Devolver la lista de costos mínimos
-    return answer
+	"""
+	# costoo mínimo hasta
+	# el estado objetivo desde el inicio
+	global grafo, costo
+	respuesta = []
+	# crear una cola de prioridad
+	cola = []
 
+	# establecer el vector de respuesta en un valor máximo
+	for i in range(len(objetivo)):
+		respuesta.append(10**8)
 
+	# insertar el índice de inicio
+	cola.append([0, inicio])
+	# mapa para almacenar nodo visitado
+	visitado = {}
+	# contador
+	contador = 0
+
+	# mientras la cola no esté vacía
+	while (len(cola) > 0):
+		# obtener el elemento superior de la
+		cola = sorted(cola)
+		p = cola[-1]
+		# eliminar el elemento
+		del cola[-1]
+		# obtener el valor original
+		p[0] *= -1
+		# verificar si el elemento es parte de
+		# la lista de objetivos
+		if (p[1] in objetivo):
+			# obtener la posición
+			índice = objetivo.index(p[1])
+			# si se alcanza un nuevo objetivo
+			if (respuesta[índice] == 10**8):
+				contador += 1
+			# si el costoo es menor
+			if (respuesta[índice] > p[0]):
+				respuesta[índice] = p[0]
+			# eliminar el elemento
+			del cola[-1]
+			cola = sorted(cola)
+			if (contador == len(objetivo)):
+				return respuesta
+
+		# verificar los nodos no visitados
+		# que están adyacentes al nodo presente
+		if (p[1] not in visitado):
+			for i in range(len(grafo[p[1]])):
+				# el valor se multiplica por -1 para que
+				# la prioridad mínima esté en la parte superior
+				cola.append( [(p[0] + costo[(p[1], grafo[p[1]][i])])* -1, grafo[p[1]][i]])
+
+		# marcar como visitado
+		visitado[p[1]] = 1
+
+	return respuesta
+
+# función principal
 if __name__ == '__main__':
-    graph, cost = {}, {}
-    graph["A"] = ["B", "C"]
-    graph["C"] = ["B", "F", "D"]
-    graph["B"] = ["F"]
-    graph["D"] = ["E"]
-    graph["E"] = ["B"]
-    graph["F"] = ["E", "G"]
-    graph["G"] = ["D"]
-    cost[("A", "B")] = 2
-    cost[("A", "C")] = 5
-    cost[("B", "F")] = 1
-    cost[("C", "B")] = 5
-    goal = [6]
-    answer = busqueda_centro_historico(goal, "A")
-    print("La ruta más corta es", answer[0])
+	
+    # crear el grafo
+    grafo, costo = [[] for i in range(8)],{}
+
+    # el grafo es una lista de listas. Cada elemento
+	
+
+    print(grafo, costo)
+	
+	
+    grafo[0].append(1)
+    grafo[0].append(3)
+    grafo[3].append(1)
+    grafo[3].append(6)
+    grafo[3].append(4)
+    grafo[1].append(6)
+    grafo[4].append(2)
+    grafo[4].append(5)
+    grafo[2].append(1)
+    grafo[5].append(2)
+    grafo[5].append(6)
+    grafo[6].append(4)
+
+    # add the costo
+    costo[(0, 1)] = 2
+    costo[(0, 3)] = 5
+    costo[(1, 6)] = 1
+    costo[(3, 1)] = 5
+    costo[(3, 6)] = 6
+    costo[(3, 4)] = 2
+    costo[(2, 1)] = 4
+    costo[(4, 2)] = 4
+    costo[(4, 5)] = 3
+    costo[(5, 2)] = 6
+    costo[(5, 6)] = 3
+    costo[(6, 4)] = 7
+
+    print(grafo, costo)
+
+    # estado del objetivo
+    objetivo = []
+
+    # se establece la meta
+    # pueden haber múltiples objetvios
+    objetivo.append(6)
+
+    # get the answer
+    answer = busqueda_general(objetivo, 0)
+	
+    # print the answer
+    print("El costo mínimo desde 0 hasta 6 es:  ",answer[0])
