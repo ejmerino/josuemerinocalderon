d13c8e03a3c96b3229dac75a49f80637276da027
Josué Merino Calderón <josuemerinocalderon11@gmail.com>
2024-05-28 10:24:58 -0500

    Add code draw lines

---
+++ Computación Gráfica/Parcial 1/Dibujar Lineas/Frminicio.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Clase28Mayo
+{
+    partial class Frminicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(435, 37);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(124, 38);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // Frminicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.btnGenerar);
+            this.MaximumSize = new System.Drawing.Size(1000, 600);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Frminicio";
+            this.Text = "Formulario Inicio";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Frminicio_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+    }
+}
---
+++ Computación Gráfica/Parcial 1/Dibujar Lineas/Frminicio.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase28Mayo
+{
+    public partial class Frminicio : Form
+    {
+
+        private int lineCount = 0;
+        private Timer timer;
+
+        public Frminicio()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 300;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lineCount++;
+            this.Invalidate(); 
+            if (lineCount >= 4)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void Frminicio_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (lineCount >= 1)
+            {
+                g.DrawLine(Pens.Black, 0, 80, 750, 450);
+            }
+            if (lineCount >= 2)
+            {
+                g.DrawLine(Pens.Red, 0, 450, 750, 80);
+            }
+            if (lineCount >= 3)
+            {
+                g.DrawLine(Pens.Green, 375, 80, 375, 450);
+            }
+            if (lineCount >= 4)
+            {
+                g.DrawLine(Pens.Blue, 0, 265, 750, 265);
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            lineCount = 0;
+            timer.Start();
+        }
+    }
+}
