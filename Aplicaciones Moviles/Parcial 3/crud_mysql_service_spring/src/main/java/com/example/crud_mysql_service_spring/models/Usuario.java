c162689e010a45d3c7b9cc01b4c55afff6cf65c3
Josué Merino Calderón <yoshichicharra11@gmail.com>
2025-02-04 12:12:56 -0500

    Add Usuario model + Usuario Repository

--- Aplicaciones Moviles/Parcial 3/crud_mysql_service_spring/src/main/java/com/example/crud_mysql_service_spring/models/Usuario.java
+++ Aplicaciones Moviles/Parcial 3/crud_mysql_service_spring/src/main/java/com/example/crud_mysql_service_spring/models/Usuario.java
@@ -1,4 +1,47 @@
 package com.example.crud_mysql_service_spring.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Usuario {
+
+    //Clave Primaria
+    @Id
+    //Especificar que el ID se genera automáticamente en la base de datos
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+
+    public Usuario(){}
+
+    public Usuario(String nombre, String email){
+        this.nombre=nombre;
+        this.email=email;
+    }
+
+    //Métodos de acceso
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- Aplicaciones Moviles/Parcial 3/crud_mysql_service_spring/src/main/java/com/example/crud_mysql_service_spring/repositories/UsuarioRepositorio.java
+++ Aplicaciones Moviles/Parcial 3/crud_mysql_service_spring/src/main/java/com/example/crud_mysql_service_spring/repositories/UsuarioRepositorio.java
@@ -1,4 +1,9 @@
 package com.example.crud_mysql_service_spring.repositories;
 
-public interface UsuarioRepositorio {
+import com.example.crud_mysql_service_spring.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Extiende JPARepository con la entidad Usuario y el tipo de la clave primaria
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+
 }
