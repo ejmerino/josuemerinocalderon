2dc712b68ba89668f2e1c94ca05949d804e8e4b5
Josué Merino Calderón <yoshichicharra11@gmail.com>
2025-02-04 11:43:21 -0500

    Add Backend Structure

---
+++ Aplicaciones Moviles/Parcial 3/crud_mysql_service_spring/src/main/java/com/example/crud_mysql_service_spring/CrudMysqlServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_mysql_service_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMysqlServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMysqlServiceSpringApplication.class, args);
+	}
+
+}
---
+++ Aplicaciones Moviles/Parcial 3/crud_mysql_service_spring/src/test/java/com/example/crud_mysql_service_spring/CrudMysqlServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_mysql_service_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMysqlServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
