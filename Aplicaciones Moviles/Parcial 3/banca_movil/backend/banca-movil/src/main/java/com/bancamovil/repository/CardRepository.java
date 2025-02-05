5c9deaf10f3d60a2056eed353963691924041921
Josué Merino Calderón <yoshichicharra11@gmail.com>
2025-02-05 18:21:37 -0500

    Change structure

--- Aplicaciones Moviles/Parcial 3/banca_movil/backend/banca-movil/src/main/java/com/bancamovil/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bancamovil.repository;
-
-import com.bancamovil.model.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
